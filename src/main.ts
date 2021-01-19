bb72f705c176be5f55f2eab1ea140aafc9f8fd68
Stephan <stephan.kop@outlook.com>
2021-01-19 10:53:13 +0100

    changed port in main.ts for heroku

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
         .build();
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('api', app, document);
-    await app.listen(process.env.PORT || 3001);
+    await app.listen(process.env.PORT || 8080);
 }
 
 bootstrap();
